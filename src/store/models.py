a81fb8f334ea2af65994e519d85016798a0b393c
Julio Sanjuan <julio.sanjuan@konfio.mx>
2022-12-13 07:46:54 -0600

    Added: GET products service

--- src/store/models.py
+++ src/store/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=256)
+    price = models.DecimalField(decimal_places=2, max_digits=16)
+    description = models.CharField(max_length=1024)
+    image_path = models.CharField(max_length=256)
+    stock = models.IntegerField()
+    created_at = models.DateTimeField( default= timezone.now )
--- src/store/urls.py
+++ src/store/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home ), #store/
-    path("users", views.users) #store/users
+    path("users", views.users), #store/users
+    path("products", views.products)
 ]
